ec4349e0e640803bee9ae8ef02eeac516be85892
suryag23 <surya_gunasekaran@comcast.com>
2026-06-15 19:50:52 +0530

    addressed the review comments

--- accelerator-home-ui/src/api/PackageManagerApi.js
+++ accelerator-home-ui/src/api/PackageManagerApi.js
@@ -45,7 +45,7 @@ export default class PackageManager {
     const err = new ThunderError(thunderCall, thunderErr);
     const errString = err.toString();
     this.ERR(errString);
-    Metrics.error(Metrics.ErrorType.OTHER, "AppPackageManager", errString, false, null)
+    Metrics.error(Metrics.ErrorType.OTHER, this.callsign, errString, false, null)
 
     throw err;
   }
@@ -54,7 +54,7 @@ export default class PackageManager {
     return this.thunder.Controller.activate(
       { callsign: this.callsign }
     ).then(() => {
-      this.INFO("AppPackageManager activated");
+      this.INFO(`${this.callsign} activated`);
       return true;
     }).catch(err => {
       this.handleThunderError(`activate(${this.callsign})`, err);
@@ -65,7 +65,7 @@ export default class PackageManager {
     return this.thunder.Controller.deactivate(
       { callsign: this.callsign }
     ).then(() => {
-      this.INFO("AppPackageManager deactivated");
+      this.INFO(`${this.callsign} deactivated`);
       return true;
     }).catch(err => {
       this.handleThunderError(`deactivate(${this.callsign})`, err);
