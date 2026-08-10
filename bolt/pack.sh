f7eeedec7e5139fb8237d2eb7d2cc037df02ec39
SudarsananComcast <Sudarsanan_Kumar@comcast.com>
2026-08-10 14:03:55 +0530

    changed command for typescript and added in package.json

--- bolt/pack.sh
+++ bolt/pack.sh
@@ -27,7 +27,7 @@ npm install
 
 npm install -g @lightningjs/cli
 
-npm install --prefix /usr/local/lib/node_modules/@lightningjs/cli typescript@5.2.2 --no-save
+sudo npm install --prefix "$(npm root -g)/@lightningjs/cli" typescript@5.2.2 --no-save
 
 lng dist
 
